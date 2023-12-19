527931de6fcf7468dc13008564325e287d3dcdac
MSI\Aloi <kasper1116.kh@gmail.com>
2023-12-19 23:18:45 +0800

    新增bb.b1與route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/bb/b1', function () {
+    return view('bb.b1');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
