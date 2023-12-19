00b3568470fa144a7a7b2bfb6335fe3a05e984cd
Fuuzuki0307 <Yaesakura716207@gmail.com>
2023-12-19 23:41:47 +0800

    新增dd.d1與route

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,10 @@ Route::get('/bb/b2', function () {
     return view('bb.b2');
 });
 
+Route::get('/dd/d1', function () {
+    return view('dd.d1');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
