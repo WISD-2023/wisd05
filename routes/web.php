8208035f3bef77fb760054d9c096a573f82f39fe
MSI\Aloi <kasper1116.kh@gmail.com>
2023-12-19 23:38:58 +0800

    加入cc.c1與route

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,10 @@ Route::get('/bb/b2', function () {
     return view('bb.b2');
 });
 
+Route::get('/cc/c1', function () {
+    return view('cc.c1');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
